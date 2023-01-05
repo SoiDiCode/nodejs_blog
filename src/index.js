ac08be8e22ec6daab492dfc0f6ab3434a56544ad
SoiDiCode <camhoang1811@gmail.com>
2023-01-05 17:51:51 +0700

    chia temple và use handlebar to create interfaceweb

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000
+
+// cấu hình lại đuôi file handlebars : .handlebars => any
+var config = {
+    extname: '.hbs'
+}
+
+// logger tiêu chuẩn(có nhiều loại logger)
+// HTTP Logger
+app.use(morgan('combined'));
+
+
+//Temple engines
+// định nghĩa handlebars = ()
+app.engine('hbs', handlebars.engine(config));
+// ứng dụng express(app) sử dụng view engine là handlebars
+app.set('view engine', 'hbs')
+// chỉ lại đường dẫn
+// nếu lỗi đổi dấu \\
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// console.log('Path', __dirname);//=> lấy ra đường dẫn chạy index.js( D:\NodeJS\F8_NodeJS\demo_nodemon\src)
+// console.log('Path', path.join(__dirname, 'resource/views'));
+
+// đây là 1 router : gồm trang điều hướng , req và res
+// chú ý : dấu => around funtion còn ko có  là function
+// khi trong thân function có 1 câu lệnh đơn thì around ko cần '{}'
+// app.get("/trang-chu",
+//     (req, res) =>
+//         res.send(
+//             "hello mọi người"
+//         ));
+
+app.get("/trang-chu", (req, res) => res.render("home"));
+
+app.get("/tin-tuc", (req, res) => res.render("news"));
+
+
+
+
+app.listen(port, () => {
+    console.log(`App is running with port is ${port}`)
+})
